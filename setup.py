8a7a7452d9d9e79066f55feeccdec6d667ddb964
Guyot François <51274081+iFrostizz@users.noreply.github.com>
2021-05-21 00:59:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "1.0.0-bugfix"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """This plugin was created to calibrate 3D printer settings easily."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "iFrostizz"
