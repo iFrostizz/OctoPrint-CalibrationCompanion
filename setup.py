bede87a36188857bf5c9b2d5a74d6845af2c9c08
Guyot François <51274081+iFrostizz@users.noreply.github.com>
2021-05-22 16:28:57 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "octoprint_calibrationcompanion"
 plugin_name = "Calibration Companion"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.0-bugfix"
+plugin_version = "0.1.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """This plugin was created to calibrate 3D printer settings easily."""
+plugin_description = """TODO"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "iFrostizz"
