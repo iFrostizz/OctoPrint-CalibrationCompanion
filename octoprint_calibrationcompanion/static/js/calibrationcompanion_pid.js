5f72728d2a6ec4b96234ecc060a35cc1b62e7ba8
Guyot François <fgg67@hotmail.fr>
2021-09-01 12:27:16 +0200

    Setting temp to 0 after PID

--- octoprint_calibrationcompanion/static/js/calibrationcompanion_pid.js
+++ octoprint_calibrationcompanion/static/js/calibrationcompanion_pid.js
@@ -109,7 +109,13 @@ $(function() {
         
         function setPidValues(pidConstants) {
             const [P, I, D] = [pidConstants[0], pidConstants[1], pidConstants[2]];
-            OctoPrint.control.sendGcode(["M301 E" + extruderIndex + " P" + P + " I" + I + " D" + D, "M500"]);
+            if (extruderIndex === -1) {
+                OctoPrint.control.sendGcode(["M301 E" + extruderIndex + " P" + P + " I" + I + " D" + D, "M500", "M140 S0"]);
+            } else if (extruderIndex === 0) {
+                OctoPrint.control.sendGcode(["M301 E" + extruderIndex + " P" + P + " I" + I + " D" + D, "M500", "M104 S0"]);
+            } else {
+                OctoPrint.control.sendGcode(["M301 E" + extruderIndex + " P" + P + " I" + I + " D" + D, "M500"]);
+            }
             let message = "New PID constants set!\nP:" + P + " I:" + I + " D:" + D + " for extruder: " + extruderIndex
             PNotifyShowMessage(message, true, 'info');
         }
